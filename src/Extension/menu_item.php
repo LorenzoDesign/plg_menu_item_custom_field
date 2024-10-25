261e84cd71e67d3117dd56651a226acd984402b8
Lorenzo Giovannini <viruzz15@gmail.com>
2024-10-25 17:57:26 +0200

    installabile

---
+++ src/Extension/menu_item.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.Menu_item
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Plugin\Fields\Menu_item\Extension;
+
+use Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Fields Menu_item Plugin
+ *
+ * @since  3.7.0
+ */
+final class Menu_item extends FieldsPlugin
+{
+}
